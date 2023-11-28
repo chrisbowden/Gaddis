6d85f2d42cb3f33b476cffe58f28b04d39d83c3e
Chris Bowden <chrisjbowden@gmail.com>
2023-11-28 11:41:24 +0800

    Updating Object Chapter (Cellphone)

---
+++ Ch10/Examples/cellphone.py
@@ -0,0 +1,11 @@
+# The CellPhone class holds data about a cell phone
+
+class CellPhone:
+
+    # The __init__ method initializes the attributes
+
+    def __init__(self, manufact, model, price):
+        self.__
+
+
+# Random Comment
