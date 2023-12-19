74c20710cc988f503043973f8d5a0a0219c2aaa3
Chris Bowden <chrisjbowden@gmail.com>
2023-12-19 08:02:10 +0800

    Updating Object Chapter (Cellphone)

--- Ch10/Examples/cellphone.py
+++ Ch10/Examples/cellphone.py
@@ -5,7 +5,48 @@ class CellPhone:
     # The __init__ method initializes the attributes
 
     def __init__(self, manufact, model, price):
-        self.__
+        self.__manufact = manufact
+        self.__model = model
+        self.__retail_price = price
+
+    # The set_manufact method accepts an argument for
+    # the phones maunfacturer
+
+    def set_manufact(self, manufact):
+        self.__manufact = manufact
+
+    # The set_model method accepts an argument for
+    # the phones model number
+
+    def set_model(self, model):
+        self.__model = model
+
+    # The set_retail_price method accepts an argument
+    # for the phones retail price
+
+    def set_retail_price(self, price):
+        self.__retail_price = price
+
+    # The get_manufact method returns the
+    # phones manufacturer
+
+    def get_manufact(self):
+        return self.__manufact
+
+    # The get model method returns the
+    # phones model number
+
+    def get_model(self):
+        return self.__model
+
+    # The get retail price method returns
+    # the phones retail price
+
+    def get_retail_price(self):
+        return self.__retail_price
+
+    
+
 
 
 # Random Comment
